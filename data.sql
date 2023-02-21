48861a71ebef0d34da216edff1f6b9dacaf3adcd
Glenda Diaz <glendadiazah@outlook.com>
2023-02-21 16:50:36 -0600

    Revert "Add commands to answer questions, and update the table"
    This reverts commit ece448422ac11729f41ec8a89ff059c81470399e.

--- data.sql
+++ data.sql
@@ -4,12 +4,3 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (2, 'Gabumon', '2018/11/15', 2, true, 8);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (3, 'Pikachu', '2021/01/07', 1, false, 15.04);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (4, 'Devimon', '2017/05/12', 5, true, 11);
-
-/* DAY 2 */
-ALTER TABLE animals ADD COLUMN species text;
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (5, 'Charmander', '2020/02/08', 0, false, -11);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (6, 'Plantnom', '2021/11/15', 2, true, -5.7);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (7, 'Squirtle', '1993/04/02', 3, false, -12.13);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (8, 'Angemon', '2005/06/12', 1, true, -45);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (9, 'Boarmon', '2005/06/07', 7, true, 20.4);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (4, 'Blossom', '1998/10/13', 4, true, 22);
--- queries.sql
+++ queries.sql
@@ -8,27 +8,3 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5
 SELECT name FROM animals WHERE neutered = true
 SELECT name FROM animals WHERE name != 'Gabumon'
 SELECT name, weight_kg FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3
-
-/* DAY 2 */
-
-UPDATE animals SET species = 'unspecified' WHERE species is null;
-ROLLBACK
-UPDATE animals SET species = 'pokemon' WHERE name LIKE '%mon';
-UPDATE animals SET species = 'digimon' WHERE species is null;
-BEGIN;
-DELETE from animals WHERE date_of_birth > '2022-01-01';
-SAVEPOINT SP1;
-UPDATE animals SET weight_kg = weight_kg*-1;
-ROLLBACK TO SP1;
-UPDATE animals SET weight_kg = weight_kg*-1 WHERE weight_kg < 0;
-COMMIT;
-
-/* Answer questions */
-
-SELECT count(name) FROM animals;
-SELECT count(*) FROM animals WHERE escape_attempts <= 0
-SELECT AVG(weight_kg) FROM animals;
-SELECT MAX(escape_attempts) FROM animals GROUP BY neutered;
-SELECT MAX(weight_kg) FROM animals GROUP BY species;
-SELECT MIN(weight_kg) FROM animals GROUP BY species;
-SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
